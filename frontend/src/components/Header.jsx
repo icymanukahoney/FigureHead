05df68dd7d9b86f88f73190395ac1365d6bf19ad
Makyla Scoullar <makylascoullar@Makylas-MacBook-Pro.local>
2023-09-13 10:46:04 +1200

    Worked on Nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+// This will be throwing an error for the time being till fixed
+import { Link } from "react-router-dom";
+import { useState } from "react";
+// This is commented out because it will be used later
+// import { useAuthContext } from "../hooks/useAuthContext";
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Header
+  return (
+    <nav>
+      <div className="nav-flex" id="nav-top">
+        <Link className="logo" to="/">
+          <img src="#" alt="#" />
+        </Link>
+        <h1>FigureHead</h1>
+        <i class="fa-solid fa-bars"></i>
+
+      </div>
+      <div className="nav-flex" id="nav-bottom"></div>
+    </nav>
+  );
+};
+
+export default Header;
