c032c04fe1bd104b2bce9d50f75127623bee2c95
Ryan Gudsell <ryangudsell@gmail.com>
2023-09-13 10:20:20 +1200

    Initial HTML and class/id set up

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-      
+    <div id="profile-page">
+      <div className='bg-image'></div>
+      <div>
+        <img src="#" alt="Background image of a figurine collection wall." />
+        <h1>ACCOUNT NAME</h1>
+        <h2>Listings</h2>
+        <div id="profile-listings">
+          <div className="profile-card">
+            <div>
+              <img src="#" alt="Image of the figurine" />
+            </div>
+            <div>
+              <p className="listing-title">
+                Dunegons & Dragons: Gelatinous Cube POP! Vinyl
+              </p>
+              <p className="listing-category">Category, Category</p>
+              <p className="listing-price">$50.00</p>
+
+              <div className="profile-listing-edit-delete">
+                <i className="fa fa-edit"></i>
+                <i className="fa fa-delete"></i>
+              </div>
+            </div>
+          </div>
+          <div id="add-listing" className="profile-card">
+            <div>
+              <h3>Create New Listing</h3>
+              <i className="fa fa-add"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
