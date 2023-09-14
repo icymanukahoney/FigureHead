1d321ebe7bbf6f293238176d886bdd15200100de
Makyla Scoullar <makylascoullar@Makylas-MacBook-Pro.local>
2023-09-15 11:14:39 +1200

    this updates the nav bar and fixes the errors on it as well as shows my slow progress on the login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useState } from "react";
 
 const Login = () => {
+  
+  // I think this is right, I'm unsure check this over using two methods
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useLogin();
+
+  // handleSubmit function
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    await login(email, password);
+  };
+
   return (
     <div>
+      <form className="login" onSubmit={handleSubmit}>
+      {/* paste the code that is broken in once merge is over */}
+      </form>
       
     </div>
   )
