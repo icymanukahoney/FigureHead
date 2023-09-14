9145969dac21187d5fbd3bd11461f303e45ae8c3
Ryan Gudsell <ryangudsell@gmail.com>
2023-09-14 22:54:06 +1200

    Added modal functionality for add and edit

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 
-const AddProduct = () => {
+const AddProduct = (props) => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    props.onFormSubmit();
+  }
+
   return (
-    <div>
-      
+    <div id="add-product-modal" className="add-edit-modals">
+      <form onSubmit={handleSubmit}>
+        <h2>ADD LISTING</h2>
+        <div>
+          <label>Name *</label>
+          <input type="text" placeholder="Add Name Here" />
+        </div>
+        <div>
+          <label>Description</label>
+          <p><textarea cols="30" rows="10" placeholder="Add Description Here"></textarea></p>
+        </div>
+        <div>
+          <label>Price *</label>
+          <input type="text" placeholder="Add Name Here" />
+        </div>
+        <div>
+          <label>Images (Up to three) *</label>
+          <input type="file" />
+        </div>
+        <div>
+          <label>Categories *</label>
+          <div className="category-flex">
+            <div>
+              <input type="checkbox" name="category" value="anime" /><label>Anime</label>
+              <input type="checkbox" name="category" value="cartoon" /><label>Cartoon</label>
+              <input type="checkbox" name="category" value="comic" /><label>Comic</label>
+            </div>
+            <div>
+              <input type="checkbox" name="category" value="game" /><label>Game</label>
+              <input type="checkbox" name="category" value="movie" /><label>Movie</label>
+              <input type="checkbox" name="category" value="tv" /><label>TV Show</label>
+            </div>
+            <div>
+              <button>SUBMIT</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AddProduct from "../components/AddProduct";
+import EditProduct from "../components/EditProduct";
 
 const Profile = () => {
+
+  // const navigate = useNavigate();
+
+  // const routeChange = () => {
+  //   let path = `/${product._id}`;
+  //   navigate(path)
+  // }
+
+  const [isAddOpen, setIsAddOpen] = useState(false)
+  const handleAddSubmit = () => {setIsAddOpen(false)}
+
+  const [isEditOpen, setIsEditOpen] = useState(false)
+  const handleEditSubmit = () => {setIsEditOpen(false)}
+
   return (
     <div id="profile-page">
       <div className='bg-image'></div>
@@ -21,7 +38,9 @@ const Profile = () => {
               <p className="listing-price">$50.00</p>
 
               <div className="profile-listing-edit-delete">
-                <i className="fa fa-pen-to-square"></i>
+                <i className="fa fa-pen-to-square" onClick={() => {
+                  setIsEditOpen(true)
+                }}></i>
                 <i className="fa fa-trash"></i>
               </div>
             </div>
@@ -94,7 +113,9 @@ const Profile = () => {
               </div>
             </div>
           </div>
-          <div id="add-listing" className="profile-card">
+          <div id="add-listing" className="profile-card" onClick={() => {
+            setIsAddOpen(true)
+          }}>
             <div>
               <h3>Create New Listing</h3>
               <i id="add-icon" className="fa fa-circle-plus"></i>
@@ -102,6 +123,8 @@ const Profile = () => {
           </div>
         </div>
       </div>
+        {isAddOpen && <AddProduct onFormSubmit={handleAddSubmit} />}
+        {isEditOpen && <EditProduct onFormSubmit={handleEditSubmit} />}
     </div>
   )
 }
